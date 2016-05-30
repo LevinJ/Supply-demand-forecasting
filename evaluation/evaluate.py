de8cb40bbba3e0763138eb4c4ff97f54f7665f4d
Levin Jian <jianzhirong@gmail.com>
2016-05-30 15:40:06 +0800

    add prediction_0 csv file

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -3,29 +3,9 @@ import numpy as np
 from datetime import datetime
 from datetime import timedelta
 
-
-class Evaluate:
+class GenerateResultCsv:
     def __init__(self):
-        self.actualDict = self.loadDict('actual.csv')
-        self.predictonDict = self.loadDict('prediction.csv')
         return
-    def loadDict(self, filename):
-        df = pd.read_csv(filename, header=None)
-        res = {}
-        for _, row in df.iterrows():
-            res[(row[0], row[1])] = row[2]
-        return res
-    def calFinalResult(self):
-        res = []
-        for key, value in self.predictonDict.iteritems():
-            actual = self.actualDict[key]
-            if actual == 0:
-                continue
-            prediction = value
-            temp = (actual - prediction)/float(actual)
-            res.append(abs(temp))
-        print "final result: {}".format(np.mean(res))
-        return np.mean(res)
     def generateTestDate(self):
         startDate = datetime.strptime('2016-01-01', '%Y-%m-%d')
         
@@ -45,9 +25,18 @@ class Evaluate:
         return res
     def generateTestDistrict(self):
         return [i+ 1 for i in range(66)]
-    def generateTestCSV(self):
+    def generatePrediction_0(self):
         testSlots = self.generateTestSlots()
-#         testDistricts = self.generateTestDistrict()
+        allOderFilePath = '../data/citydata/season_1/training_data/order_data/temp/allorders.csv'
+        df = pd.read_csv(allOderFilePath)
+        df = df.loc[df['time_slotid'].isin(testSlots)]
+        #map 2016-01-22-1 to 2016-01-22-001
+        df['timeslotrank'] = df['time_slotid'].map(lambda x: "-".join(x.split('-')[:3] + [x.split('-')[-1].zfill(3)]))
+        df = df.sort_values(by = ['timeslotrank','start_district_id'])
+        df.to_csv('prediction_0.csv', columns=['start_district_id', 'time_slotid', 'missed_request'], header=None, index=None)
+        return
+    def generateActual_0(self):
+        testSlots = self.generateTestSlots()
         allOderFilePath = '../data/citydata/season_1/training_data/order_data/temp/allorders.csv'
         df = pd.read_csv(allOderFilePath)
         df = df.loc[df['time_slotid'].isin(testSlots)]
@@ -56,9 +45,43 @@ class Evaluate:
         df = df.sort_values(by = ['timeslotrank','start_district_id'])
         df.to_csv('actual_0.csv', columns=['start_district_id', 'time_slotid', 'missed_request'], header=None, index=None)
         return
+    
+    
+class Evaluate(GenerateResultCsv):
+    def __init__(self):
+        GenerateResultCsv.__init__(self)
+        return
+    def loadResultFiles(self, testSetNum):
+
+        actualFile = 'actual_' + str(testSetNum) + '.csv'
+        predictionFile = 'prediction_' + str(testSetNum) + '.csv'
+        self.actualDict = self.loadDict(actualFile)
+        self.predictonDict = self.loadDict(predictionFile)
+        return
+    
+    def loadDict(self, filename):
+        df = pd.read_csv(filename, header=None)
+        res = {}
+        for _, row in df.iterrows():
+            res[(row[0], row[1])] = row[2]
+        return res
+    def calFinalResult(self, testSetNum):
+        self.loadResultFiles(testSetNum)
+        res = []
+        for key, value in self.predictonDict.iteritems():
+            actual = self.actualDict[key]
+            if actual == 0:
+                continue
+            prediction = value
+            temp = (actual - prediction)/float(actual)
+            res.append(abs(temp))
+        print "final result: {}".format(np.mean(res))
+        return np.mean(res)
+    
     def run(self):
-#         self.calFinalResult()
-        self.generateTestCSV()
+#         self.generateActual_0()
+#         self.generatePrediction_0()
+        self.calFinalResult(0)
         return
     
 
