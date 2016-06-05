8f3c699308a36ff47be3bd6708c10648c0188188
Levin Jian <jianzhirong@gmail.com>
2016-06-05 14:59:58 +0800

    visualize for weather data in training set

--- exploredata/visualization.py
+++ exploredata/visualization.py
@@ -3,14 +3,17 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set(color_codes=True)
 from utility.datafilepath import g_singletonDataFilePath
+from weather import ExploreWeather
+import numpy as np
+import math
 
 
-
-class visualizeOrder(ExploreOrder):
+class visualizeOrder(ExploreOrder, ExploreWeather):
     def __init__(self):
         ExploreOrder.__init__(self)
 #         self.df, _ = self.loadGapData(g_singletonDataFilePath.getGapCsv_Train())
-        self.df, _ = self.loadGapData(g_singletonDataFilePath.getGapCsv_Test1())
+#         self.df, _ = self.loadGapData(g_singletonDataFilePath.getGapCsv_Test1())
+        self.weathdf = self.load_weatherdf(g_singletonDataFilePath.getTrainDir())
         return
     def drawGapDistribution(self):
         self.df[self.df['gap'] < 10]['gap'].hist(bins=50)
@@ -27,9 +30,26 @@ class visualizeOrder(ExploreOrder):
         ax2.bar(res.index.map(lambda x: x[11:]), res.values)
         plt.show()
         return
+    def show_weather_bydate(self):
+        by_date = self.weathdf.groupby('time_date')
+        size = len(by_date)
+        col_len = row_len = math.ceil(math.sqrt(size))
+        count = 1
+        for name, group in by_date:
+            ax=plt.subplot(row_len, col_len, count)
+            temp = np.empty(group['time_id'].shape[0])
+            temp.fill(2)
+            ax.bar(group['time_id'], temp, width=1)
+            ax.set_title(name)
+            count = count + 1
+#             plt.bar(group['time_id'], np.full(group['time_id'].shape[0], 5), width=1)
+            
+        plt.show()
+        return
     def run(self):
+        self.show_weather_bydate()
 #         self.drawGapDistribution()
-        self.drawGapCorrelation()
+#         self.drawGapCorrelation()
         return
     
 
