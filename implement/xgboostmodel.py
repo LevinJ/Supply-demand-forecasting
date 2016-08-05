b271b15ed1f6542a7cbfd6ba1da5ea52a3e8a438
Levin Jian <jianzhirong@gmail.com>
2016-08-05 14:35:58 +0800

    simplify xgboost base model

--- implement/xgboostmodel.py
+++ implement/xgboostmodel.py
@@ -7,7 +7,6 @@ import numpy as np
 from utility.runtype import RunType
 from utility.datafilepath import g_singletonDataFilePath
 from preprocess.splittrainvalidation import HoldoutSplitMethod
-import matplotlib.pyplot as plt
 import xgboost as xgb
 from evaluation.sklearnmape import mean_absolute_percentage_error_xgboost
 from evaluation.sklearnmape import mean_absolute_percentage_error
@@ -17,104 +16,60 @@ from evaluation.sklearnmape import mean_absolute_percentage_error_xgboost_cv
 from utility.xgbbasemodel import XGBoostBase
 
 
-
-
 class DidiXGBoostModel(XGBoostBase, PrepareData, XGBoostGridSearch):
     def __init__(self):
         PrepareData.__init__(self)
         XGBoostGridSearch.__init__(self)
         XGBoostBase.__init__(self)
+        self.do_cross_val = None
         
-
-        self.do_cross_val = True
-#         self.holdout_split = HoldoutSplitMethod.IMITTATE_TEST2_MIN #[78]    train-mape:-0.373462+0.00492894    test-mape:-0.46214+0.0114662
-#         self.holdout_split = HoldoutSplitMethod.IMITTATE_TEST2_PLUS1 # [79]    train-mape:-0.396072+0.00363566    test-mape:-0.459982+0.0100845
-#         self.holdout_split = HoldoutSplitMethod.IMITTATE_TEST2_PLUS3 #[78]    train-mape:-0.411597+0.00219856    test-mape:-0.454906+0.0124385
-#         self.holdout_split = HoldoutSplitMethod.IMITTATE_TEST2_PLUS4#[78]    train-mape:-0.411597+0.00219856    test-mape:-0.454906+0.0124385
-#         self.holdout_split = HoldoutSplitMethod.IMITTATE_TEST2_PLUS6# [82]    train-mape:-0.421504+0.00191357    test-mape:-0.453868+0.0116971
-#         self.holdout_split = HoldoutSplitMethod.IMITTATE_TEST2_FULL#[81]    train-mape:-0.428109+0.000703088    test-mape:-0.451429+0.0114696
         return
-
-    def run_croos_validation(self):
-         
-        # Use default parameters only
-        param = {'max_depth':14, 'eta':0.02, 'silent':1, 'objective':'reg:linear' }
-        
-        #Run 100 rounds, just to ensure it's sufficiently trained
-        num_boost_round = 100
+    def set_xgb_parameters(self):
         early_stopping_rounds = 3
-         
-        # specify validations set to watch performance
-        xgb.cv(param, self.dtrain_cv, num_boost_round=num_boost_round,
-               callbacks=[xgb.callback.print_evaluation(show_stdv=True),xgb.callback.early_stop(early_stopping_rounds)], **self.folds_params)
-
-        return 
-    def run_train_validation(self):
-
-        # specify parameters via map, definition are same as c++ version
-        param = {'max_depth':14, 'eta':0.02, 'silent':1, 'objective':'reg:linear' }
-#         param = { 'silent':1}
-         
-        # specify validations set to watch performance
-        evals  = [(self.dtrain,'train'),(self.dvalidation,'eval')]
-        num_boost_round = 100
-        early_stopping_rounds=3
-        
-        xgb.train(param, self.dtrain, num_boost_round=num_boost_round, evals = evals, early_stopping_rounds=early_stopping_rounds,**self.folds_params)
-         
-        print "features used:\n {}".format(self.usedFeatures)
-         
+        self.xgb_params = {'max_depth':14, 'eta':0.02, 'silent':1, 'objective':'reg:linear' }
+        self.xgb_learning_params = {
+                                    'num_boost_round': 100,
+                                    'callbacks':[xgb.callback.print_evaluation(show_stdv=True),xgb.callback.early_stop(early_stopping_rounds)],
+                                    'feval':mean_absolute_percentage_error_xgboost_cv}
+        if self.do_cross_val == False:
+            self.xgb_learning_params['feval'] = mean_absolute_percentage_error_xgboost
         return
-    def get_model_input(self):
-        
-        if self.do_cross_val is None or self.do_cross_val: 
-            # for cross validation
-            features,labels,self.cv_folds = self.getFeaturesLabel()
-            self.dtrain_cv  = xgb.DMatrix(features, label= labels,feature_names=features.columns)
-            #for grid search
-            self.folds_params = {'folds':self.cv_folds, 'feval':mean_absolute_percentage_error_xgboost_cv}
-            return
-        
-        # for train validation
-        x_train, y_train,x_validation,y_validation = self.get_train_validationset()
-        self.dtrain = xgb.DMatrix(x_train, label= y_train,feature_names=x_train.columns)
-        self.dvalidation = xgb.DMatrix(x_validation, label= y_validation,feature_names=x_validation.columns)
-        
-        self.folds_params = {'feval':mean_absolute_percentage_error_xgboost}
-        
-        return
-    def adjust_intial_param(self):
+    def get_paramgrid_1(self):
         """
         This method must be overriden by derived class when its objective is not reg:linear
         """
-        param = {'max_depth':6, 'eta':0.1,  'min_child_weight':1,'silent':1, 
-                 'objective':'reg:linear','colsample_bytree':0.8,'subsample':0.8,'min_child_weight':1 }
-        return param
-    
-    def adjust_param(self, param_grid):
+        param_grid = {'max_depth':[6], 'eta':[0.1],  'min_child_weight':[1],'silent':[1], 
+                 'objective':['reg:linear'],'colsample_bytree':[0.8],'subsample':[0.8]}
+        return param_grid
+     
+    def get_paramgrid_2(self, param_grid):
         """
         This method must be overriden by derived class if it intends to fine tune parameters
         """
         self.ramdonized_search_enable = False
-        self.randomized_search_n_iter = 2
+        self.randomized_search_n_iter = 3
         self.grid_search_display_result = True
-
-        param_grid['eta'] = [0.01, 0.05]  #[54]    train-mape:-0.450673+0.00167039    test-mape:-0.45734+0.00530681
-        param_grid['max_depth'] = [13] #[78]    train-mape:-0.406378+0.00244131    test-mape:-0.456578+0.0100904
+ 
+#         param_grid['eta'] = [0.01]  #[54]    train-mape:-0.450673+0.00167039    test-mape:-0.45734+0.00530681
+#         param_grid['max_depth'] = range(13,25) #[78]    train-mape:-0.406378+0.00244131    test-mape:-0.456578+0.0100904
+        param_grid['max_depth'] = [3,4]
+        param_grid['eta'] = [0.01,0.1] # 0.459426+0.00518875
+#         param_grid['subsample'] = [0.5] #0.458935+0.00522205
+#         param_grid['eta'] = [0.005] #0.457677+0.00526401
         return param_grid
-    
-    def adjust_cv_param(self):
+     
+    def get_learning_params(self):
         """e
         This method must be overriden by derived class if it intends to fine tune parameters
         """
         num_boost_round = 100
         early_stopping_rounds = 3
         
-        kwargs = {'num_boost_round':num_boost_round, 
+
+         
+        kwargs = {'num_boost_round':num_boost_round, 'feval':mean_absolute_percentage_error_xgboost_cv,
                   'callbacks':[xgb.callback.print_evaluation(show_stdv=True),xgb.callback.early_stop(early_stopping_rounds)]}
         return kwargs
-    
-
 
 if __name__ == "__main__":   
     obj= DidiXGBoostModel()
--- utility/xgbbasemodel.py
+++ utility/xgbbasemodel.py
@@ -2,6 +2,7 @@ import xgboost as xgb
 from sklearn.grid_search import ParameterGrid
 from sklearn.grid_search import ParameterSampler
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 
@@ -10,61 +11,40 @@ class XGBoostBase(object):
         self.do_cross_val = True
         return
     def run(self):
-        self.get_model_input()
+#         self.get_model_input()
         if self.do_cross_val is None:
             return self.run_grid_search()
         if self.do_cross_val:
             return self.run_croos_validation()
         return self.run_train_validation()
     def run_croos_validation(self):
-         
-        # Use default parameters only
-        param = {'silent':1}
         
-        #Run 100 rounds, just to ensure it's sufficiently trained
-        num_boost_round = 100
-        early_stopping_rounds = 3
-         
-        # specify validations set to watch performance
-        xgb.cv(param, self.dtrain_cv, num_boost_round=num_boost_round,
-               callbacks=[xgb.callback.print_evaluation(show_stdv=True),xgb.callback.early_stop(early_stopping_rounds)], **self.folds_params)
+        features,labels,cv_folds = self.getFeaturesLabel()
+        dtrain_cv  = xgb.DMatrix(features, label= labels,feature_names=features.columns)
+        self.set_xgb_parameters()
 
+        # specify validations set to watch performance
+        xgb.cv(self.xgb_params, dtrain_cv, folds=cv_folds, **self.xgb_learning_params)
         return 
+    def set_xgb_parameters(self):
+        self.xgb_params = {'silent':1}
+        self.xgb_learning_params = {}
+        return
     def run_train_validation(self):
-
-        # specify parameters via map, definition are same as c++ version
-        param = {'max_depth':14, 'eta':0.02, 'silent':1, 'objective':'reg:linear' }
-#         param = { 'silent':1}
-         
-        # specify validations set to watch performance
-        evals  = [(self.dtrain,'train'),(self.dvalidation,'eval')]
-        num_boost_round = 100
-        early_stopping_rounds=3
-        
-        xgb.train(param, self.dtrain, num_boost_round=num_boost_round, evals = evals, early_stopping_rounds=early_stopping_rounds,**self.folds_params)
-         
-        print "features used:\n {}".format(self.usedFeatures)
-         
-        return
-    def get_model_input(self):
-        
-        if self.do_cross_val is None or self.do_cross_val: 
-            # for cross validation
-            features,labels,self.cv_folds = self.getFeaturesLabel()
-            self.dtrain_cv  = xgb.DMatrix(features, label= labels,feature_names=features.columns)
-            #for grid search
-            self.folds_params = None
-            return
-        
-        # for train validation
         x_train, y_train,x_validation,y_validation = self.get_train_validationset()
-        self.dtrain = xgb.DMatrix(x_train, label= y_train,feature_names=x_train.columns)
-        self.dvalidation = xgb.DMatrix(x_validation, label= y_validation,feature_names=x_validation.columns)
+        dtrain = xgb.DMatrix(x_train, label= y_train,feature_names=x_train.columns)
+        dvalidation = xgb.DMatrix(x_validation, label= y_validation,feature_names=x_validation.columns)
+        self.set_xgb_parameters()
         
-        self.folds_params = None
-        
-       
+        evals=[(dtrain,'train'),(dvalidation,'eval')]
+        model = xgb.train(self.xgb_params, dtrain, evals=evals, **self.xgb_learning_params)
+        xgb.plot_importance(model)
+        plt.show()
+         
+        print "features used:\n {}".format(self.get_used_features())
+         
         return
+
     
 class XGBoostGridSearch(object):
     def __init__(self):
@@ -78,34 +58,12 @@ class XGBoostGridSearch(object):
         
         return
 
-    def adjust_intial_param(self):
-        """
-        This method must be overriden by derived class when its objective is not reg:linear
-        """
-        param = {'max_depth':6, 'eta':0.1,  'min_child_weight':1,'silent':1, 
-                 'objective':'reg:linear','colsample_bytree':0.8,'subsample':0.8,'min_child_weight':1 }
-        return param
-    def adjust_param(self, param_grid):
-        """
-        This method must be overriden by derived class if it intends to fine tune parameters
-        """
-        self.ramdonized_search_enable = False
-        self.randomized_search_n_iter = 2
-        self.grid_search_display_result = False
-
-        param_grid['eta'] = [0.01]  #[54]    train-mape:-0.450673+0.00167039    test-mape:-0.45734+0.00530681
-        param_grid['max_depth'] = [13] #[78]    train-mape:-0.406378+0.00244131    test-mape:-0.456578+0.0100904
-        return param_grid
-    def adjust_cv_param(self):
-        """
-        This method must be overriden by derived class if it intends to fine tune parameters
-        """
-        num_boost_round = 10
-        early_stopping_rounds = 3
-        
-        kwargs = {'num_boost_round':num_boost_round, 
-                  'callbacks':[xgb.callback.print_evaluation(show_stdv=True),xgb.callback.early_stop(early_stopping_rounds)]}
-        return kwargs
+    def get_paramgrid_1(self):
+        pass
+    def get_paramgrid_2(self, param_grid):
+        pass
+    def get_learning_params(self):
+        pass
     def __get_param_iterable(self, param_grid):
         if self.ramdonized_search_enable:
             parameter_iterable = ParameterSampler(param_grid,
@@ -116,32 +74,28 @@ class XGBoostGridSearch(object):
                  
         return parameter_iterable
     def __get_param_grid(self):
-        param = self.adjust_intial_param()
-        param_grid = {}
-        for key, value in param.iteritems():
-            param_grid[key] = [value]
-        
-        param_grid = self.adjust_param(param_grid) 
+        param_grid = self.get_paramgrid_1()
+        param_grid = self.get_paramgrid_2(param_grid) 
         return param_grid
-    def __get_kwargs(self, folds_params):
+    def __get_kwargs(self):
         # specify validations set to watch performance
-        kwargs = self.adjust_cv_param()
-        # add the cv related parameters
-        for key, value in folds_params.iteritems():
-            kwargs[key] = value 
+        kwargs = self.get_learning_params()
         kwargs['callbacks'].append(self.__get_early_stop_metric())
         return kwargs
     def run_grid_search(self):
         """
         This method is called by derived class to start grid search process
         """
+        features,labels,cv_folds = self.getFeaturesLabel()
+        dtrain_cv  = xgb.DMatrix(features, label= labels,feature_names=features.columns)
            
         parameter_iterable = self.__get_param_iterable(self.__get_param_grid())  
-        kwargs = self.__get_kwargs(self.folds_params)
+        kwargs = self.__get_kwargs()
         for param in parameter_iterable:
             print param
-            bst = xgb.cv(param, self.dtrain_cv, **kwargs)
+            bst = xgb.cv(param, dtrain_cv, folds=cv_folds,**kwargs)
             self.__add_to_resultset(param, bst)
+#             xgb.callback.early_stop.cleanup()# clear the callb ack state
         self.__disp_result() 
         return
     def __get_early_stop_metric(self):
